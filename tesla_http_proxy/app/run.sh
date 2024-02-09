7eef17ec20d09b900feacd35b0958c85538556a0
Daniel Nitsche <github@nitsche.id.au>
2024-02-10 07:33:07 +1100

    Update run.sh

--- tesla_http_proxy/app/run.sh
+++ tesla_http_proxy/app/run.sh
@@ -14,10 +14,10 @@ if [ -f /share/tesla/private.pem ]; then
 else
   printf "\n### Generating keypair ###\n"
   openssl ecparam -genkey -name prime256v1 -noout > private.pem
-	openssl ec -in private.pem -pubout > public.pem
+  openssl ec -in private.pem -pubout > public.pem
   printf "\n### Requesting vehicle access ###\n"
   printf "\n### Tap your NFC card or keyfob on the center console and then tap 'Confirm' on the vehicle screen. ###\n"
- 	/root/bin/tesla-control -vin $VIN -ble add-key-request public.pem owner cloud_key
+  /root/bin/tesla-control -vin $VIN -ble add-key-request public.pem owner cloud_key
   # TODO check for success
   mkdir -p /share/tesla
   cp private.pem /share/tesla/private.pem
